65ce9a6439d68c8c457d5319cee4e71d6d7700cd
Przemysław Dudek <dudek.assa@gmail.com>
2021-03-23 08:25:42 +0000

    FIX: 23 03 2021

--- Main.java
+++ Main.java
@@ -6,32 +6,40 @@ class Main {
     int age = 15;
 
     //tworzenie regału
-    int[] ageArray = new int[5];
+    int[] ageArray = new int[6];
+    String[] nameArray = new String[6];
 
     //Mikołaj wiek
     ageArray[0] = 15;
-
-       //Michał wiek
-    ageArray[2] = 15;
+    nameArray[0] = "Mikołaj";
 
     //Marysia i Julia wiek
     ageArray[1] = 14;
+    nameArray[1] = "Julia i Marysia";
+
+    //Michał wiek
+    ageArray[2] = 15;
+    nameArray[2] = "Michał";
 
     //Martyna dodała wiek
     ageArray[3] = age;
+    nameArray[3] = "Martyna";
 
     //Przemek wiek
     ageArray[4] = 35;
+    nameArray[4] = "Przemek";
 
-    System.out.println("Wiek Przemek: " + ageArray[4]);
+    //Wiktoria i Patrycja [5]
+    ageArray[5] = 15;
+    nameArray[5] = "Wiktoria i Patrycja";
 
-    System.out.println("Wiek Julki: " + ageArray[1]);
+    System.out.println("Pętla: ");
 
-    System.out.println("Wiek Martyny: "+ ageArray[3]);
+    for (int i = 0; i < 6; i++ ) {
+      System.out.println("Wartość i: " + i + ", wiek: " + ageArray[i]  + ", imię: " + nameArray[i]);
+    }
 
-    System.out.println("Wiek Michała: "+ ageArray[2]);
-
-    System.out.println("Wiek Mikołaja: "+ ageArray[0]);
+    System.out.println("Koniec pętli: ");
 
   }
 }
